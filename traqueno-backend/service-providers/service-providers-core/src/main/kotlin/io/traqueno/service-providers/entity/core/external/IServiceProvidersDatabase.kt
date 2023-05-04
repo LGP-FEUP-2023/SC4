6c137450ad9e5bf900d00f1921890aec7d4096be
matejicko <matogrochal01@gmail.com>
2023-05-04 18:50:55 +0200

    database implementation details

--- traqueno-backend/service-providers/service-providers-core/src/main/kotlin/io/traqueno/service-providers/entity/core/external/IServiceProvidersDatabase.kt
+++ traqueno-backend/service-providers/service-providers-core/src/main/kotlin/io/traqueno/service-providers/entity/core/external/IServiceProvidersDatabase.kt
@@ -4,6 +4,6 @@ import io.traqueno.service.providers.entity.core.model.ServiceProvider
 
 interface IServiceProvidersDatabase {
     fun putServiceProvider(serviceProvider: ServiceProvider): Boolean
-    fun getServiceProviderByName(name: String): ServiceProvider?
+    fun searchServiceProvidersWithNameLike(name: String): ServiceProvider[]?
     fun getServiceProviderById(id: UUID): ServiceProvider?
 }
--- traqueno-backend/service-providers/service-providers-external/service-providers-external-database/src/main/kotlin/io/traqueno/service/providers/database/entity/ServiceProviderDBO.kt
+++
@@ -1,9 +0,0 @@
-package io.traqueno.service.providers.database.entity
-
-data class ServiceProviderDBO(
-    val id: UUID,
-    var domain: String,
-    var topic: String,
-    var contact: Contact,
-    var openingHours: OpeningHours
-)
