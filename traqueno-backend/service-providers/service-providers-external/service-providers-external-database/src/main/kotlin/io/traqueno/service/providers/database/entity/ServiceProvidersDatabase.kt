c871e83dc33a205332618261b32bb4d45e5835f1
matejicko <matogrochal01@gmail.com>
2023-05-04 18:00:04 +0200

    database implementation

--- traqueno-backend/service-providers/service-providers-external/service-providers-external-database/src/main/kotlin/io/traqueno/service/providers/database/entity/ServiceProvidersDatabase.kt
+++ traqueno-backend/service-providers/service-providers-external/service-providers-external-database/src/main/kotlin/io/traqueno/service/providers/database/entity/ServiceProvidersDatabase.kt
@@ -1,47 +1,45 @@
 package io.traqueno.service.providers.database.entity
 
 import com.mongodb.client.MongoCollection
-import io.traqueno.user.entity.core.external.IUserAuthDatabase
-import io.traqueno.user.entity.core.model.User
+import io.traqueno.service.providers.entity.core.external.IServiceProvidersDatabase
+import io.traqueno.service.providers.entity.core.model.ServiceProvider
 import org.litote.kmongo.eq
 import org.litote.kmongo.findOne
 import java.util.*
 import javax.enterprise.context.ApplicationScoped
 
 @ApplicationScoped
-class ServiceProvidersDatabase(private val userCollection: MongoCollection<UserDBO>) : IServiceProvidersDatabase, android.os.Parcelable {
+class ServiceProvidersDatabase(private val serviceProvidersCollection: MongoCollection<ServiceProvider>) : IServiceProvidersDatabase, android.os.Parcelable {
+      
+    override fun getServiceProviderById(id: UUID): = serviceProvidersCollection.findOne(ServiceProvider::id eq id)
 
-    override fun getUserByEmail(email: String) = userCollection.findOne(UserDBO::email eq email)?.toUser()
-
-    override fun getUserById(userId: String) = userCollection.findOne(UserDBO::userId eq userId)?.toUser()
-
-    override fun putUser(newUser: User): Boolean {
+    override fun searchServiceProvidersWithNameLike(name: String): ServiceProvider?{
+        val results = runCatching {
+            serviceProvidersCollection.find()
+        }
+    }
+    
+    override fun putServiceProvider(serviceProvider: ServiceProvider): Boolean{
         val result = runCatching {
-            newUser.toUserDBO().let(userCollection::insertOne).wasAcknowledged()
+            serviceProvider.let(serviceProvidersCollection::insertOne).wasAcknowledged()
         }
         return result.getOrNull() ?: false
     }
 
     override fun writeToParcel(parcel: android.os.Parcel, flags: Int) {
-
     }
 
     override fun describeContents(): Int {
         return 0
     }
 
-    companion object CREATOR : android.os.Parcelable.Creator<UserAuthDatabase> {
-        override fun createFromParcel(parcel: android.os.Parcel): UserAuthDatabase {
-            return UserAuthDatabase(parcel)
+    companion object CREATOR : android.os.Parcelable.Creator<ServiceProvidersDatabase> {
+        override fun createFromParcel(parcel: android.os.Parcel): ServiceProvidersDatabase {
+            return ServiceProvidersDatabase(parcel)
         }
 
-        override fun newArray(size: Int): Array<UserAuthDatabase?> {
+        override fun newArray(size: Int): Array<ServiceProvidersDatabase?> {
             return arrayOfNulls(size)
         }
     }
 }
-
-fun UserDBO.toUser() =
-    User(UUID.fromString(this.userId), this.firstName, this.lastName, this.email, this.hashedPassword)
-
-fun User.toUserDBO() = UserDBO(this.userId.toString(), this.firstName, this.lastName, this.email, this.hashedPassword)
